88db1c408bf00b9bb4575c4b4643ddadaf22134c
katieskipper <skipper.katie@gmail.com>
2012-08-11 11:07:59 -0700

    progress

--- about_exceptions.rb
+++ about_exceptions.rb
@@ -6,10 +6,10 @@ class AboutExceptions < EdgeCase::Koan
   end
 
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
@@ -20,7 +20,7 @@ class AboutExceptions < EdgeCase::Koan
       result = :exception_handled
     end
 
-    assert_equal __, result
+    assert_equal :exception_handled, result
 
     assert_equal __, ex.is_a?(StandardError), "Should be a Standard Error"
     assert_equal __, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
